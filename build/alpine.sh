3cb8adeb1a5fc487dad562052f078aa64e69aa03
Raphael Castro <raphael.castro@webjump.com.br>
2019-06-12 13:47:51 -0300

    ajustando novo alpine.sh

--- build/alpine.sh
+++ build/alpine.sh
@@ -2,7 +2,7 @@ echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && 
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
 
 apk update && \
-	apk add build-base \
+  apk add build-base \
   libxml2-dev \
   libxslt-dev \
   postgresql-dev \
