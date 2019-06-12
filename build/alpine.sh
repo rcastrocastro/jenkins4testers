b0acbe0c03cd5d106dca29390a9d7ba4f62e6cfb
Raphael Castro <raphael.castro@webjump.com.br>
2019-06-12 13:38:26 -0300

    ajustando novamente o alpine.sh

--- build/alpine.sh
+++ build/alpine.sh
@@ -3,8 +3,8 @@ echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositorie
 
 apk update && \
 	apk add build-base \
-    libxml2-dev \
-    libxslt-dev \
-    postgresql-dev \
-    curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
-    && rm -rf /var/cache/apk/*
+  libxml2-dev \
+  libxslt-dev \
+  postgresql-dev \
+  curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
+  && rm -rf /var/cache/apk/*
