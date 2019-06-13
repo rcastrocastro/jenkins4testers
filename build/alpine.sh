251f3d3df46da89cf739141b0574945160562bf3
Raphael Castro <raphael.castro@webjump.com.br>
2019-06-13 17:50:27 -0300

    testes13

--- build/alpine.sh
+++ build/alpine.sh
@@ -6,6 +6,6 @@ apk update && \
   libxml2-dev \
   libxslt-dev \
   postgresql-dev \
-  curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
+  curl unzip libexif udev chromium-73-gcc-6.patch chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
   udev \
   && rm -rf /var/cache/apk/*
