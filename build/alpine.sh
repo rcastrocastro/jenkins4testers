09e0050ea4ca24bd4ed98414ed4f0ec2253726de
Raphael Castro <raphael.castro@webjump.com.br>
2019-06-13 13:24:11 -0300

    teste 5

--- build/alpine.sh
+++ build/alpine.sh
@@ -7,7 +7,7 @@ apk update && \
   libxslt-dev \
   postgresql-dev \
   # curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
-  install google-chrome-stable xvfb unzip libxi6 libgconf-2-4 \
+  install google-chrome-stable xvfb unzip libgconf-2-4 \
   wget "https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip" \
   unzip chromedriver_linux64.zip \
   mv chromedriver /usr/bin/chromedriver \
